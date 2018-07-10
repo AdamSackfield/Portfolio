29e09b4a605ae8774bf752248bf23f9532c882b0
AdamSackfield <=>
2018-07-10 22:18:43 +0100

    Added contact success

---
+++ client/src/components/Success/Success.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import classes from './Success.scss'
+
+const Success = () => {
+  return (
+    <div className={classes.Success}>
+      <p>
+        Message success. Please allow up to 24 hours for a response.
+      </p>
+    </div>
+  )
+}
+
+export default Success
