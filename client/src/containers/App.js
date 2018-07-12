3837addcbd7acd0d69af662cc58fff29549e2f9e
AdamSackfield <=>
2018-07-12 01:00:17 +0100

    Bug fix

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,22 +1,18 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom';
 
-import Layout from '../components/Layout/Layout'
-import Success from '../components/Success/Success'
-import ErrorComp from '../components/Error/Error'
+import Layout from '../components/Layout/Layout';
+import Success from '../components/Success/Success';
+import ErrorComp from '../components/Error/Error';
 
 class App extends Component {
 	render() {
 		return (
-			<div className={classes.Main}>
-				{/* <Navigation /> */}
-
-				<Switch>
-					<Route path='/' exact component={Layout} />
-					<Route path='/success' exact component={Success} />
-					<Route path='/error' exact component={ErrorComp} />
-				</Switch>
-			</div>
+			<Switch>
+				<Route path="/" exact component={Layout} />
+				<Route path="/success" exact component={Success} />
+				<Route path="/error" exact component={ErrorComp} />
+			</Switch>
 		);
 	}
 }
